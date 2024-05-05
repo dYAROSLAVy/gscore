1ae3bfb6434b69ada333db26968a324b3cbd0f4f
dYAROSLAVy <yadsp@yandex.ru>
2024-05-05 18:24:46 +0300

    feat: add manage code query

--- src/entities/codes/api.ts
+++ src/entities/codes/api.ts
@@ -2,6 +2,8 @@ import { API_URL } from "@/shared/constants/api-url";
 import { HTTP_METHODS } from "@/shared/constants/http-methods";
 import { baseApi } from "@/shared/redux/base-api";
 
+export type CodeManagePutParams = { token?: string; codesIds?: number[], subscribeId?: number}
+
 export const licensesApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     activateLicense: builder.mutation<{}, { token?: string; code?: string }>({
@@ -15,7 +17,18 @@ export const licensesApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: ["Subscribes"],
     }),
+    codeManage: builder.mutation<void, CodeManagePutParams>({
+      query: ({ token, ...body }) => ({
+        url: API_URL.codeManage,
+        method: HTTP_METHODS.put,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body,
+      }),
+      invalidatesTags: ["Subscribes"],
+    }),
   }),
 });
 
-export const { useActivateLicenseMutation } = licensesApi;
+export const { useActivateLicenseMutation, useCodeManageMutation } = licensesApi;
